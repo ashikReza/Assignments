3925d1da72c0b0c76037282f2956b799deccde57
Ashik044 <rezaashik044@gmail.com>
2024-01-29 18:34:59 +0600

    done

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -72,59 +72,64 @@ export default function TaskList() {
             </th>
           </tr>
         </thead>
-
-        <tbody>
-          {filteredTasks.map((task) => (
-            <tr
-              className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
-              key={task.id}
-            >
-              <td className="cursor-pointer">
-                <button onClick={() => handleFavoriteToggle(task.id)}>
-                  {task.isFavorited ? (
-                    <FaStar color="yellow" size={22} />
-                  ) : (
-                    <FaStar color="gray" size={22} />
-                  )}
-                </button>
-              </td>
-              <td className="flex">{task.title}</td>
-              <td>{task.description}</td>
-              <td>
-                <ul className="flex justify-center gap-1.5 flex-wrap">
-                  {task.tags.map((tag, index) => (
-                    <li key={tag}>
-                      <span
-                        className={`inline-block h-5 whitespace-nowrap rounded-[45px] px-2.5 text-sm capitalize text-[#F4F5F6] ${
-                          tagColors[index % tagColors.length]
-                        }`}
-                      >
-                        {tag}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              </td>
-              <td className="text-center">{task.priority}</td>
-              <td>
-                <div className="flex items-center justify-center space-x-3">
-                  <button
-                    className="text-red-500"
-                    onClick={() => handleDeleteTask(task.id)}
-                  >
-                    Delete
+        {filteredTasks.length > 0 ? (
+          <tbody>
+            {filteredTasks.map((task) => (
+              <tr
+                className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
+                key={task.id}
+              >
+                <td className="cursor-pointer">
+                  <button onClick={() => handleFavoriteToggle(task.id)}>
+                    {task.isFavorited ? (
+                      <FaStar color="yellow" size={22} />
+                    ) : (
+                      <FaStar color="gray" size={22} />
+                    )}
                   </button>
-                  <button
-                    className="text-blue-500"
-                    onClick={() => handleEditTask(task)}
-                  >
-                    Edit
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+                </td>
+                <td className="flex">{task.title}</td>
+                <td>{task.description}</td>
+                <td>
+                  <ul className="flex justify-center gap-1.5 flex-wrap">
+                    {task.tags.map((tag, index) => (
+                      <li key={tag}>
+                        <span
+                          className={`inline-block h-5 whitespace-nowrap rounded-[45px] px-2.5 text-sm capitalize text-[#F4F5F6] ${
+                            tagColors[index % tagColors.length]
+                          }`}
+                        >
+                          {tag}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </td>
+                <td className="text-center">{task.priority}</td>
+                <td>
+                  <div className="flex items-center justify-center space-x-3">
+                    <button
+                      className="text-red-500"
+                      onClick={() => handleDeleteTask(task.id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="text-blue-500"
+                      onClick={() => handleEditTask(task)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <p className="absolute left-1/2 transform -translate-x-1/2 text-white text-center font-bold text-lg sm:text-xl">
+            No task found
+          </p>
+        )}
       </table>
 
       <div className="">
