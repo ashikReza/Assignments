2c127dc16c98409728dcf5e185d8a96caffd85ce
Ashik044 <rezaashik044@gmail.com>
2024-01-30 13:49:20 +0600

    disabled hover done

--- src/components/taskAction.jsx
+++ src/components/taskAction.jsx
@@ -42,7 +42,7 @@ export default function TaskAction() {
         Add Task
       </button>
       <button
-        className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold"
+        className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold disabledBtn"
         onClick={handleDeleteAllClick}
         disabled={tasks.length === 0}
       >
