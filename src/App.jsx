ed452401f09ce0e3b0fb4659b897df0745a7f980
Ashik044 <rezaashik044@gmail.com>
2024-02-07 20:10:43 +0600

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import Header from "./Header";
-import Hero from "./Hero";
-import TaskBoard from "./components/taskBoard";
+
 import Footer from "./Footer";
 
-import { TaskProvider } from "./contexts/TaskContext";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-
-      <TaskProvider>
-        <TaskBoard />
-      </TaskProvider>
-
-      <Footer />
-
-      <ToastContainer position="top-right" autoClose={3000} />
-    </>
-  );
+  return <>Hello world</>;
 }
 
 export default App;
--- src/contexts/utils/taskUtils.jsx
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { TaskContext } from "../TaskContext"; 
-
-export const useTaskContext = () => useContext(TaskContext);
