36a217aae7c00261fcb132740fd1e4eb0654c8c1
Ashik044 <rezaashik044@gmail.com>
2024-02-28 12:23:26 +0600

    all Model state update done

--- src/components/CreateBlogModal.jsx
+++ src/components/CreateBlogModal.jsx
@@ -4,74 +4,82 @@
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 export default function CreateBlog({ onClose }) {
+  const handleModalClick = (e) => {
+    // Check if the click occurred outside of the content area
+    if (e.target.classList.contains("Modal")) {
+      onClose();
+    }
+  };
+
   return (
-    <>
-      <section className="h-screen flex justify-center fixed top-0 left-0 w-full bg-slate-800/50 backdrop-blur-sm z-50 ">
-        <div className="container bg-black text-white rounded m-4 sm:top-32">
-          {/* <!-- Form Input field for creating Blog Post --> */}
-          <form action="#" method="POST" className="createBlog">
-            <button
-              className="fixed top-6 sm:top-10 right-6 sm:right-36 cursor-pointer"
-              onClick={onClose}
-            >
-              <IoIosCloseCircleOutline className="size-10" />
-            </button>
+    <section
+      className="h-screen flex justify-center fixed top-0 left-0 w-full bg-slate-800/50 backdrop-blur-sm z-50 Modal"
+      onClick={handleModalClick}
+    >
+      <div className="container bg-black text-white rounded m-4 sm:top-32">
+        {/* <!-- Form Input field for creating Blog Post --> */}
+        <form action="#" method="POST" className="createBlog">
+          <button
+            className="fixed top-6 sm:top-10 right-6 sm:right-36 cursor-pointer"
+            onClick={onClose}
+          >
+            <IoIosCloseCircleOutline className="size-10" />
+          </button>
 
-            <div className="grid place-items-center bg-slate-600/20 h-[150px] rounded-md my-4">
-              <div className="flex items-center gap-4 hover:scale-110 transition-all cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-                <p>Upload Your Image</p>
-              </div>
+          <div className="grid place-items-center bg-slate-600/20 h-[150px] rounded-md my-4">
+            <div className="flex items-center gap-4 hover:scale-110 transition-all cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                />
+              </svg>
+              <p>Upload Your Image</p>
             </div>
-            <div className="mb-6">
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Enter your blog title"
-              />
-            </div>
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Enter your blog title"
+            />
+          </div>
 
-            <div className="mb-6">
-              <input
-                type="text"
-                id="tags"
-                name="tags"
-                placeholder="Your Comma Separated Tags Ex. JavaScript, React, Node, Express,"
-              />
-            </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              id="tags"
+              name="tags"
+              placeholder="Your Comma Separated Tags Ex. JavaScript, React, Node, Express,"
+            />
+          </div>
 
-            <div className="mb-6">
-              <textarea
-                id="content"
-                name="content"
-                placeholder="Write your blog content"
-                rows="8"
-              ></textarea>
-            </div>
+          <div className="mb-6">
+            <textarea
+              id="content"
+              name="content"
+              placeholder="Write your blog content"
+              rows="8"
+            ></textarea>
+          </div>
 
-            <a
-              href="./createBlog.html"
-              className="bg-indigo-600 text-white px-6 py-2 md:py-3 rounded-md hover:bg-indigo-700 transition-all duration-200"
-            >
-              Create Blog
-            </a>
-          </form>
-        </div>
-      </section>
-    </>
+          <a
+            href="./createBlog.html"
+            className="bg-indigo-600 text-white px-6 py-2 md:py-3 rounded-md hover:bg-indigo-700 transition-all duration-200"
+          >
+            Create Blog
+          </a>
+        </form>
+      </div>
+    </section>
   );
 }
--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -4,45 +4,53 @@ import taiulwind from "../assets/blogs/taiulwind-cn-thumb.jpg";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 export default function Search({ onClose }) {
-  return (
-    <>
-      <section className="fixed left-0 top-0 w-full h-full grid place-items-center bg-slate-800/50 backdrop-blur-sm z-50 ">
-        <div className="relative w-6/12 mx-auto bg-slate-900 p-4 border border-slate-600/50 rounded-lg shadow-lg shadow-slate-400/10">
-          <div>
-            <h3 className="font-bold text-xl pl-2 text-slate-400 my-2">
-              Search for Your Desire Blogs
-            </h3>
-            <input
-              type="text"
-              placeholder="Start Typing to Search"
-              className="w-full bg-transparent p-2 text-base text-white outline-none border-none rounded-lg focus:ring focus:ring-indigo-600"
-            />
-          </div>
+  const handleModalClick = (e) => {
+    // Check if the click occurred outside of the content area
+    if (e.target.classList.contains("Modal")) {
+      onClose();
+    }
+  };
 
-          <div>
-            <h3 className="text-slate-400 font-bold mt-6">Search Results</h3>
-            <div className="my-4 divide-y-2 divide-slate-500/30 max-h-[440px] overflow-y-scroll overscroll-contain">
-              <div className="flex gap-6 py-2">
-                <img className="h-28 object-contain" src={taiulwind} alt="" />
-                <div className="mt-2">
-                  <h3 className="text-slate-300 text-xl font-bold">
-                    Style your components with TailwindCSS
-                  </h3>
-                  <p className="mb-6 text-sm text-slate-500 mt-1">
-                    Aenean eleifend ante maecenas pulvinar montes lorem et pede
-                    dis dolor pretium donec dictum. Vici consequat justo enim.
-                    Venenatis eget adipiscing luctus lorem.
-                  </p>
-                </div>
+  return (
+    <section
+      className="fixed left-0 top-0 w-full h-full grid place-items-center bg-slate-800/50 backdrop-blur-sm z-50 Modal"
+      onClick={handleModalClick}
+    >
+      <div className="relative w-6/12 mx-auto bg-slate-900 p-4 border border-slate-600/50 rounded-lg shadow-lg shadow-slate-400/10">
+        <div>
+          <h3 className="font-bold text-xl pl-2 text-slate-400 my-2">
+            Search for Your Desire Blogs
+          </h3>
+          <input
+            type="text"
+            placeholder="Start Typing to Search"
+            className="w-full bg-transparent p-2 text-base text-white outline-none border-none rounded-lg focus:ring focus:ring-indigo-600"
+          />
+        </div>
+
+        <div>
+          <h3 className="text-slate-400 font-bold mt-6">Search Results</h3>
+          <div className="my-4 divide-y-2 divide-slate-500/30 max-h-[440px] overflow-y-scroll overscroll-contain">
+            <div className="flex gap-6 py-2">
+              <img className="h-28 object-contain" src={taiulwind} alt="" />
+              <div className="mt-2">
+                <h3 className="text-slate-300 text-xl font-bold">
+                  Style your components with TailwindCSS
+                </h3>
+                <p className="mb-6 text-sm text-slate-500 mt-1">
+                  Aenean eleifend ante maecenas pulvinar montes lorem et pede
+                  dis dolor pretium donec dictum. Vici consequat justo enim.
+                  Venenatis eget adipiscing luctus lorem.
+                </p>
               </div>
             </div>
           </div>
-
-          <button className="absolute top-5 right-5" onClick={onClose}>
-            <IoIosCloseCircleOutline size={30} />
-          </button>
         </div>
-      </section>
-    </>
+
+        <button className="absolute top-5 right-5" onClick={onClose}>
+          <IoIosCloseCircleOutline size={30} />
+        </button>
+      </div>
+    </section>
   );
 }
