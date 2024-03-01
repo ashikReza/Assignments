53fa4fd673f2801258f21916535d1afaa8155d2b
Ashik044 <rezaashik044@gmail.com>
2024-03-01 11:56:36 +0600

    App done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@
 
 import React from "react";
 
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-import Header from "./components/common/Header.jsx";
 
 import Home from "./pages/home.jsx";
 
