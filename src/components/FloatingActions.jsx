e22026c7d1723529c52df78bfaac2303828b27f9
Ashik044 <rezaashik044@gmail.com>
2024-03-02 15:37:51 +0600

    add icon on FloatingAcion

--- src/components/FloatingActions.jsx
+++ src/components/FloatingActions.jsx
@@ -1,24 +1,36 @@
 /* eslint-disable react/no-unknown-property */
-import likeIcon from "../assets/icons/like.svg";
-import heartIcon from "../assets/icons/heart.svg";
-import commentIcon from "../assets/icons/comment.svg";
+// import likeIcon from "../assets/icons/like.svg";
+// import heartIcon from "../assets/icons/heart.svg";
+// import commentIcon from "../assets/icons/comment.svg";
+
+import { AiOutlineLike } from "react-icons/ai";
+import { AiFillLike } from "react-icons/ai";
+
+import { FaRegHeart } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa6";
+
+import { FaRegMessage } from "react-icons/fa6";
+import { FaMessage } from "react-icons/fa6";
 
 export default function FloatingActions() {
   return (
     <div class="floating-action">
       <ul class="floating-action-menus">
         <li>
-          <img src={likeIcon} alt="like" />
+          <AiOutlineLike size={20} color="white" />
+          <AiFillLike size={20} color="white" />
           <span>10</span>
         </li>
 
         <li>
           {/* <!-- There is heart-filled.svg in the icons folder --> */}
-          <img src={heartIcon} alt="Favourite" />
+          <FaRegHeart size={20} color="white" />
+          <FaHeart size={20} color="red" />
         </li>
         <a href="#comments">
           <li>
-            <img src={commentIcon} alt="Comments" />
+            <FaRegMessage size={20} color="white" />
+            <FaMessage size={20} color="white" />
             <span>3</span>
           </li>
         </a>
