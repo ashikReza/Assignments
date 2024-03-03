dce2abf8024ef2e1c5a8bcab212d586502ea599b
Ashik044 <rezaashik044@gmail.com>
2024-03-03 12:32:46 +0600

    popular blog  done

--- src/components/blog feed/PopularBlogs.jsx
+++ src/components/blog feed/PopularBlogs.jsx
@@ -1,55 +1,60 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+
+import useToken from "../../hooks/useToken.js";
+
+import { Link } from "react-router-dom";
+
 export default function PopularBlogs() {
+  const [popularBlogs, setPopularBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { api } = useToken();
+
+  useEffect(() => {
+    const fetchPopularBlogs = async () => {
+      try {
+        const response = await api.get("http://localhost:3000/blogs/popular");
+
+        setPopularBlogs(response.data.blogs);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching popular blogs:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchPopularBlogs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="sidebar-card">
       <h3 className="text-slate-300 text-xl lg:text-2xl font-semibold">
         Most Popular 👍️
       </h3>
 
-      <ul className="space-y-5 my-5">
-        <li>
-          <h3 className="text-slate-400 font-medium hover:text-slate-300 transition-all cursor-pointer">
-            How to Auto Deploy a Next.js App on Ubuntu from GitHub
-          </h3>
-          <p className="text-slate-600 text-sm">
-            by
-            <a href="./profile.html">Saad Hasan</a>
-            <span>·</span> 100 Likes
-          </p>
-        </li>
-
-        <li>
-          <h3 className="text-slate-400 font-medium hover:text-slate-300 transition-all cursor-pointer">
-            How to Auto Deploy a Next.js App on Ubuntu from GitHub
-          </h3>
-          <p className="text-slate-600 text-sm">
-            by
-            <a href="./profile.html">Saad Hasan</a>
-            <span>·</span> 100 Likes
-          </p>
-        </li>
-
-        <li>
-          <h3 className="text-slate-400 font-medium hover:text-slate-300 transition-all cursor-pointer">
-            How to Auto Deploy a Next.js App on Ubuntu from GitHub
-          </h3>
-          <p className="text-slate-600 text-sm">
-            by
-            <a href="./profile.html">Saad Hasan</a>
-            <span>·</span> 100 Likes
-          </p>
-        </li>
-
-        <li>
-          <h3 className="text-slate-400 font-medium hover:text-slate-300 transition-all cursor-pointer">
-            How to Auto Deploy a Next.js App on Ubuntu from GitHub
-          </h3>
-          <p className="text-slate-600 text-sm">
-            by
-            <a href="./profile.html">Saad Hasan</a>
-            <span>·</span> 100 Likes
-          </p>
-        </li>
-      </ul>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="space-y-5 my-5">
+          {popularBlogs.map((blog) => (
+            <li key={blog.id}>
+              <>
+                <Link to={`/singleBlog/${blog.id}`}>
+                  <h3 className="text-slate-400 font-medium hover:text-slate-300 transition-all cursor-pointer">
+                    {blog.title}
+                  </h3>
+                </Link>
+                <p className="text-slate-600 text-sm">
+                  by {blog.author.firstName} {blog.author.lastName}
+                  <span>·</span> {blog.likes.length} Likes
+                </p>
+              </>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
