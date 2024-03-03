46a61ca2b19aee5ee4957582e00b4bbf14fdff78
Ashik044 <rezaashik044@gmail.com>
2024-03-03 12:14:08 +0600

    done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,18 +8,21 @@ import Footer from "../components/common/Footer";
 import { useAuth } from "../hooks/useAuth";
 
 import BlogsContent from "../components/blog feed/BlogsContent.jsx";
-import Sidebar from "../components/blog feed/Sidebar.jsx";
+
+import PopularBlogs from "../components/blog feed/PopularBlogs.jsx";
+import FavoriteBlogs from "../components/blog feed/FavoriteBlogs.jsx";
 
 export default function Home() {
   // const { auth } = useAuth();
   // console.log(auth);
 
   return (
-    <motion.div 
-    initial={{ opacity: 0, y: -50 }}
-    animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0, y: -50 }}
-    transition={{ duration: 0.5 }}>
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.5 }}
+    >
       <section className="w-full bg-[#030317] px-4 pt-10">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-7 gap-4">
@@ -27,7 +30,11 @@ export default function Home() {
             <BlogsContent />
 
             {/* <!-- Sidebar --> */}
-            <Sidebar />
+            <div className="md:col-span-2 h-full w-full space-y-5">
+              <PopularBlogs />
+
+              <FavoriteBlogs />
+            </div>
           </div>
         </div>
       </section>
