2ea154a3217d77964096bd7d5b9909eb412a1c2b
Ashik044 <rezaashik044@gmail.com>
2024-03-06 10:34:45 +0600

    Infinity scroll done with insertion observer

--- src/components/blog feed/BlogsContent.jsx
+++ src/components/blog feed/BlogsContent.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { HiDotsVertical } from "react-icons/hi";
 import ActionMenuModal from "./ActionMenuModal.jsx";
@@ -7,12 +7,13 @@ import { useAuth } from "../../hooks/useAuth.js";
 import useToken from "../../hooks/useToken.js";
 import { useBlogs } from "../../hooks/useBlogs.js";
 import { actions } from "../../actions/index.js";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { toast } from "react-toastify";
 
 export default function BlogsContent() {
   const [openBlogId, setOpenBlogId] = useState(null);
   const [hasMore, setHasMore] = useState(true); // Track if there are more blogs to load
+  const sentinelRef = useRef(null);
+  const [page, setPage] = useState(1); // Track the current page number
 
   const toggleModal = (blogId) => {
     setOpenBlogId(blogId === openBlogId ? null : blogId);
@@ -24,8 +25,7 @@ export default function BlogsContent() {
 
   useEffect(() => {
     dispatch({ type: actions.blogs.FETCH_BLOGS_REQUEST });
-    fetchBlogs(1); // Fetch the first page of blogs
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchBlogs(page); // Fetch the first page of blogs
   }, [api, dispatch]);
 
   const fetchBlogs = async (page) => {
@@ -45,6 +45,8 @@ export default function BlogsContent() {
         if (!response.data.blogs.length) {
           toast.success("All blogs loaded");
         }
+        // Increase the page number for the next fetch
+        setPage(page + 1);
       }
     } catch (error) {
       dispatch({
@@ -56,107 +58,118 @@ export default function BlogsContent() {
 
   const fetchMoreBlogs = () => {
     // Fetch the next page of blogs
-    fetchBlogs(state.page + 1);
+    fetchBlogs(page);
   };
 
-  console.log(state.blogs);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const sentinel = entries[0];
+        if (sentinel.isIntersecting && hasMore) {
+          fetchMoreBlogs();
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
+
+    if (sentinelRef.current) {
+      observer.observe(sentinelRef.current);
+    }
+
+    return () => {
+      if (sentinelRef.current) {
+        observer.unobserve(sentinelRef.current);
+      }
+    };
+  }, [hasMore, page]);
 
   return (
     <div className="space-y-3 md:col-span-5">
-      <InfiniteScroll
-        dataLength={state.blogs.length}
-        next={fetchMoreBlogs}
-        hasMore={hasMore}
-        loader={<p className="text-white">Loading...</p>}
-        endMessage={<p>All blogs loaded</p>}
-      >
-        {state.blogs.map((blog) => (
-          <div key={blog.id} className="blog-card">
-            <img
-              className="blog-thumb"
-              src={`${import.meta.env.VITE_SERVER_BLOG_URL}/${blog.thumbnail}`}
-              alt=""
-            />
-            <div className="mt-2 relative">
-              <Link to={`/singleBlog/${blog.id}`}>
-                <h3 className="text-slate-300 text-xl lg:text-2xl">
-                  {blog.title}
-                </h3>
-              </Link>
+      {state.blogs.map((blog) => (
+        <div key={blog.id} className="blog-card">
+          <img
+            className="blog-thumb"
+            src={`${import.meta.env.VITE_SERVER_BLOG_URL}/${blog.thumbnail}`}
+            alt=""
+          />
+          <div className="mt-2 relative">
+            <Link to={`/singleBlog/${blog.id}`}>
+              <h3 className="text-slate-300 text-xl lg:text-2xl">
+                {blog.title}
+              </h3>
+            </Link>
 
-              <p className="mb-6 text-base text-slate-500 mt-1">
-                {blog.content}
-              </p>
+            <p className="mb-6 text-base text-slate-500 mt-1">{blog.content}</p>
 
-              <div className="flex justify-between items-center">
-                <div className="flex items-center capitalize space-x-2">
-                  
-                  {blog.author.avatar ? (
-                    <img
-                      src={`${import.meta.env.VITE_SERVER_AVATAR_URL}/${
-                        blog.author.avatar
-                      }`}
-                      alt=""
-                      className="avater-img"
-                    />
-                  ) : (
-                    <div className="avater-img bg-blue-600 text-white">
-                      {blog.author.firstName
-                        ? blog.author.firstName[0].toUpperCase()
-                        : " "}
-                    </div>
-                  )}
-
-                  <div>
-                    <h5 className="text-slate-500 text-sm">
-                      <Link to={`/singleBlog/${blog.id}`}>
-                        {blog.author.firstName} {blog.author.lastName}
-                      </Link>
-                    </h5>
-                    <div className="flex items-center text-xs text-slate-700">
-                      <span>
-                        {new Date(blog.createdAt).toLocaleDateString("en-US", {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        })}
-                      </span>
-                    </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center capitalize space-x-2">
+                {blog.author.avatar ? (
+                  <img
+                    src={`${import.meta.env.VITE_SERVER_AVATAR_URL}/${
+                      blog.author.avatar
+                    }`}
+                    alt=""
+                    className="avater-img"
+                  />
+                ) : (
+                  <div className="avater-img bg-blue-600 text-white">
+                    {blog.author.firstName
+                      ? blog.author.firstName[0].toUpperCase()
+                      : " "}
                   </div>
-                </div>
+                )}
 
-                <div className="text-sm px-2 py-1 text-slate-700">
-                  <span>{blog.likes.length} Likes</span>
+                <div>
+                  <h5 className="text-slate-500 text-sm">
+                    <Link to={`/singleBlog/${blog.id}`}>
+                      {blog.author.firstName} {blog.author.lastName}
+                    </Link>
+                  </h5>
+                  <div className="flex items-center text-xs text-slate-700">
+                    <span>
+                      {new Date(blog.createdAt).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </span>
+                  </div>
                 </div>
               </div>
 
-              {auth.user.id === blog.author.id ? (
-                <>
-                  {/* <!-- action dot --> */}
-                  <div className="absolute right-0 top-0">
-                    <button onClick={() => toggleModal(blog.id)}>
-                      <HiDotsVertical color="white" />
-                    </button>
-
-                    {/* <!-- Action Menus Popup --> */}
-                    {openBlogId === blog.id && (
-                      <ActionMenuModal blogId={blog.id} />
-                    )}
-                  </div>
-
-                  {/* <!-- action dot ends --> */}
-                </>
-              ) : (
-                <></>
-              )}
+              <div className="text-sm px-2 py-1 text-slate-700">
+                <span>{blog.likes.length} Likes</span>
+              </div>
             </div>
+
+            {auth.user.id === blog.author.id ? (
+              <>
+                {/* <!-- action dot --> */}
+                <div className="absolute right-0 top-0">
+                  <button onClick={() => toggleModal(blog.id)}>
+                    <HiDotsVertical color="white" />
+                  </button>
+
+                  {/* <!-- Action Menus Popup --> */}
+                  {openBlogId === blog.id && (
+                    <ActionMenuModal blogId={blog.id} />
+                  )}
+                </div>
+
+                {/* <!-- action dot ends --> */}
+              </>
+            ) : (
+              <></>
+            )}
           </div>
-        ))}
-      </InfiniteScroll>
+        </div>
+      ))}
+      <div ref={sentinelRef}></div>
+      {hasMore && <p>Blogs Loading...</p>}
     </div>
   );
 }
-
-
-
-
